f115aa6f709279730088e83ab01ab6be9c661be3
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-20 18:19:48 +0100

    added RESET case to the reducer

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -1,7 +1,9 @@
+import initialState from "./initialState";
 
 const reducer = (state, action) => {
     switch (action.type) {
         case "SOMETHING": return {...state};
+        case "RESET": return initialState;
         default: return state;
     }
 }
