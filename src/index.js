12a7e6caf35dd08663eae4258f168271593410cf
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-20 16:32:40 +0100

    imported store and wrapped the App component with Provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
+import store from "./data/store";
 import App from './App';
 
 ReactDOM.render(
