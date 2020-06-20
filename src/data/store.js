535a38b6f75f218f54b811e16ce5058fa84ae0a9
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-20 17:24:32 +0100

    created state file inside the actions directory

--- src/data/store.js
+++ src/data/store.js
@@ -5,7 +5,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducer,
     initialState,
-    composeEnhancers(applyMiddleware(thunk))
+    composeEnhancers(applyMiddleware())
 );
 
 export default store;
