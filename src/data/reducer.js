69919e7319601d5f9f3de58fbdfc8e9ad8a9dc70
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-21 12:09:14 +0100

    imported intialState into reducer.js

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -1,4 +1,6 @@
 import initialState from "./initialState";
+//importing initialState to make it available to the reducer
+import initialState from "./initialState";
 
 const reducer = (state, action) => {
     switch (action.type) {
