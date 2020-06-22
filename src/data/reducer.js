7ee0655db1f7dc42142069d0977130a2eea7b202
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-22 15:30:23 +0100

    created a function in reducer that shuffles an array randomly

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -7,14 +7,35 @@ const addPlayerReducer = (state, action) => {
         ...state,
         playersPool: [...state.playersPool, action.playerName]
     }
-}
+};
+
+// Fisher–Yates shuffle
+const shuffleReducer = (array) => {
+
+    let newIndex;
+    let temporaryIndex;
+
+    for (let i = array.length - 1; i > 0; i =- 1) {
+
+        // generate new random index for the value
+        newIndex = Math.floor(Math.random() * (i + 1));
+
+        // swap the values between the indexes
+        temporaryIndex = array[i];
+        array[i] = array[newIndex];
+        array[newIndex] = temporaryIndex;
+    }
+
+    return array;
+};
 
 const reducer = (state, action) => {
     switch (action.type) {
         case "ADD_PLAYER": return addPlayerReducer(state, action);
+        case "GENERATE_TEAMS": return {...state};
         case "RESET": return initialState;
         default: return state;
     }
-}
+};
 
 export default reducer;
