d99c0b576046d89fda11a12572e88e39eb8cd9c8
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-23 15:47:18 +0100

    added a logo to home-page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import logo from './../../assets/images/ball.png';
 
 const Home = () => (
     <Jumbotron
@@ -11,7 +12,7 @@ const Home = () => (
             color: "white"
         }}
     >
-        <h1>Welcome to Football Team Generator!</h1>
+        <h1>Welcome to Football Team Generator!<img src={ logo } alt="football" width="150" height="150"/></h1>
         <p>
             This is a simple hero unit, a simple jumbotron-style component for calling
             extra attention to featured content or information.
