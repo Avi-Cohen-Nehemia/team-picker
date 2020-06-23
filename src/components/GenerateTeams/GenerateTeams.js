4c1d1165dc2d29b2dbd452af6ff69ad45b4a1e4f
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-23 12:29:12 +0100

    Generate Teams button is now disabled if playersPool < 6 || not even

--- src/components/GenerateTeams/GenerateTeams.js
+++ src/components/GenerateTeams/GenerateTeams.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 
-const GenerateTeams = ({ handleGenerateTeams }) => (
+const GenerateTeams = ({ handleGenerateTeams, conditionsNotMet }) => (
     <Button
         variant="success"
         onClick={ handleGenerateTeams }
+        disabled={ conditionsNotMet }
     >
         Generate Teams
     </Button>
--- src/components/GenerateTeams/index.js
+++ src/components/GenerateTeams/index.js
@@ -2,10 +2,16 @@ import { connect } from "react-redux";
 import { generateTeams } from "../../data/actions/state";
 import GenerateTeams from "./GenerateTeams";
 
+const mapStateToProps = (state) => {
+    return {
+        conditionsNotMet: state.playersPool.length < 6 || state.playersPool.length % 2 !== 0,
+    };
+};
+
 const mapDispatchToProps = (dispatch) => {
     return {
         handleGenerateTeams: () => dispatch(generateTeams()),
     }
 }
 
-export default connect(null, mapDispatchToProps)(GenerateTeams);
+export default connect(mapStateToProps, mapDispatchToProps)(GenerateTeams);
