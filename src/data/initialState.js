261467346f7babafcb83cfdde7e5d94eaf60f9fa
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-25 09:08:35 +0100

    made initial state more detailed to be able to represent customization

--- src/data/initialState.js
+++ src/data/initialState.js
@@ -1,7 +1,17 @@
 export const initialState = {
     playersPool: [],
-    teamA:[],
-    teamB:[],
+    teamA: {
+        players: [],
+        name: "Team A",
+        color: null,
+        totalSkillLevel: 0
+    },
+    teamB: {
+        players: [],
+        name: "Team B",
+        color: null,
+        totalSkillLevel: 0
+    },
 };
 
 export default initialState;
