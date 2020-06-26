e3b8e9af495222ab8b75614492357defff72bef6
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-26 10:00:49 +0100

    added and centered a football pitch image in the pitch-page

--- src/components/FootballPitch/index.js
+++ src/components/FootballPitch/index.js
@@ -3,10 +3,18 @@ import footballPitch from './../../assets/images/footballPitch.jpg';
 
 const FootballPitch = () => (
     <>
-        <div>
+        <div
+            className="d-flex justify-content-center align-items-center"
+        >
             <img
                 src={ footballPitch }
                 alt="football pitch"
+                style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)"
+                }}
             />
         </div>
     </>
