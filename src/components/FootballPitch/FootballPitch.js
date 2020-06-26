f49260424b486a552b54ab8dfc725f88e47eb065
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-26 13:27:33 +0100

    change pitch-page style

--- src/components/FootballPitch/FootballPitch.js
+++ src/components/FootballPitch/FootballPitch.js
@@ -6,19 +6,35 @@ import kit2 from './../../assets/images/kit2.png';
 import kit3 from './../../assets/images/kit3.png';
 
 const FootballPitch = ({ teamAPlayers, teamBPlayers, teamAName, teamBName, teamAKit, teamBKit, teamAColor, teamBColor }) => (
-    <>
-        <div className="d-flex justify-content-between">
+        <div
+            className="d-flex justify-content-between align-items-center"
+            style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+            }}
+        >      
             <Jumbotron
                 style={{
                     position: "relative",
                     opacity: 0.7,
                     backgroundColor: "black",
-                    top: "20vh",
-                    right: "7rem"
+                    marginRight: "2rem",
+                    width: "18rem",
+                    height: "20rem"
                 }}
             >
-                <div>
-                    <h1 style={{backgroundColor: teamAColor }}>{ teamAName }</h1>
+                <div className="d-flex flex-column align-items-center justify-content-center">
+                    <h1 
+                        className="px-3 mb-4"
+                        style={{
+                            backgroundColor: teamAColor,
+                            textAlign: "center"
+                        }}
+                    >
+                        { teamAName }
+                    </h1>
                     <img
                         src={ teamAKit === 1 ? kit1 : (teamAKit === 2 ? kit2 : kit3) }
                         height="100"
@@ -31,45 +47,43 @@ const FootballPitch = ({ teamAPlayers, teamBPlayers, teamAName, teamBName, teamA
                     />
                 </div>
             </Jumbotron>
-            <Jumbotron
-                style={{
-                    position: "relative",
-                    opacity: 0.7,
-                    backgroundColor: "black",
-                    top: "20vh",
-                    left: "7rem"
-                }}
-            >
-                <div>
-                    <h1 style={{backgroundColor: teamBColor }}>{ teamBName }</h1>
-                    <img
-                        src={ teamBKit === 1 ? kit1 : (teamBKit === 2 ? kit2 : kit3) }
-                        height="100"
-                        width="100"
-                        alt="team kit"
-                        style={{
-                            backgroundColor: teamBColor,
-                            borderRadius: "5px"
-                        }}
-                    />
-                </div>
-            </Jumbotron>
-        </div>
         <img
             src={ footballPitch }
             alt="football pitch"
-            style={{
-                position: "absolute",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-            }}
         />
-
-        <div>
-
+        <Jumbotron
+            style={{
+                position: "relative",
+                opacity: 0.7,
+                backgroundColor: "black",
+                marginLeft: "2rem",
+                width: "18rem",
+                height: "20rem"
+            }}
+        >
+            <div className="d-flex flex-column align-items-center justify-content-center">
+                <h1 
+                    className="px-3 mb-4"
+                    style={{
+                        backgroundColor: teamBColor,
+                        textAlign: "center"
+                    }}
+                >
+                    { teamBName }
+                </h1>
+                <img
+                    src={ teamBKit === 1 ? kit1 : (teamBKit === 2 ? kit2 : kit3) }
+                    height="100"
+                    width="100"
+                    alt="team kit"
+                    style={{
+                        backgroundColor: teamBColor,
+                        borderRadius: "5px"
+                    }}
+                />
+            </div>
+        </Jumbotron>
         </div>
-    </>
 );
 
 export default FootballPitch;
