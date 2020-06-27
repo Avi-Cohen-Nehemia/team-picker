006eac4a90a896d3e7ea432236011005392bddca
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-27 10:18:37 +0100

    refactored players positions successfuly

--- src/components/TeamAPositions/TeamAPositions.js
+++ src/components/TeamAPositions/TeamAPositions.js
@@ -1,70 +1,21 @@
 import React from "react";
 import PlayerIcon from "../PlayerIcon";
 
-import "./../../assets/css/team-a-positions.css"
+import "./../../assets/css/players-positions.css"
 
 // team A poistions on the pitch-page
 const TeamAPositions = ({ teamAPlayers, teamAColor, teamAKit }) => (
     <div>
-        <div className="player-1">
-            <PlayerIcon
-                playerName={ teamAPlayers[0].name }
-                teamColor={ teamAColor }
-                teamKit={ teamAKit }
-                playerSkillLevel={ teamAPlayers[0].skillLevel }
-            />
-        </div>
-        <div className="player-2">
-            <PlayerIcon
-                playerName={ teamAPlayers[1].name }
-                teamColor={ teamAColor }
-                teamKit={ teamAKit }
-                playerSkillLevel={ teamAPlayers[1].skillLevel }
-            />
-        </div>
-        <div className="player-3">
-            <PlayerIcon
-                playerName={ teamAPlayers[2].name }
-                teamColor={ teamAColor }
-                teamKit={ teamAKit }
-                playerSkillLevel={ teamAPlayers[2].skillLevel }
-            />
-        </div>
-
-
-        {/* the rest of the players icons will appear only if these players were created */}
-        { teamAPlayers.length >= 4 ?
-            <div className="player-4">
+        { teamAPlayers.map((player, index) => (
+            <div key={ index } className={ "player-a-" + (index + 1) }>
                 <PlayerIcon
-                    playerName={ teamAPlayers[3].name }
+                    playerName={ player.name }
                     teamColor={ teamAColor }
                     teamKit={ teamAKit }
-                    playerSkillLevel={ teamAPlayers[3].skillLevel }
+                    playerSkillLevel={ player.skillLevel }
                 />
             </div>
-        : null }
-
-        { teamAPlayers.length >= 5 ?
-            <div className="player-5">
-                <PlayerIcon
-                    playerName={ teamAPlayers[4].name }
-                    teamColor={ teamAColor }
-                    teamKit={ teamAKit }
-                    playerSkillLevel={ teamAPlayers[4].skillLevel }
-                />
-            </div>
-        : null }
-
-        { teamAPlayers.length === 6 ?
-            <div className="player-6">
-                <PlayerIcon
-                    playerName={ teamAPlayers[5].name }
-                    teamColor={ teamAColor }
-                    teamKit={ teamAKit }
-                    playerSkillLevel={ teamAPlayers[5].skillLevel }
-                />
-            </div>
-        : null }
+        )) }
     </div>
 );
 
--- src/components/TeamBPositions/TeamBPositions.js
+++ src/components/TeamBPositions/TeamBPositions.js
@@ -1,102 +1,21 @@
 import React from "react";
 import PlayerIcon from "../PlayerIcon";
 
+import "./../../assets/css/players-positions.css"
+
 // team B poistions on the pitch-page
 const TeamBPositions = ({ teamBPlayers, teamBColor, teamBKit }) => (
     <div>
-        <div
-            style={{
-                position: "absolute",
-                top: "41%",
-                right: "23%"
-            }}
-        >
-            <PlayerIcon
-                playerName={ teamBPlayers[0].name }
-                teamColor={ teamBColor }
-                teamKit={ teamBKit }
-                playerSkillLevel={ teamBPlayers[0].skillLevel }
-            />
-        </div>
-        <div
-            style={{
-                position: "absolute",
-                top: "16%",
-                right: "32%"
-            }}
-        >
-            <PlayerIcon
-                playerName={ teamBPlayers[1].name }
-                teamColor={ teamBColor }
-                teamKit={ teamBKit }
-                playerSkillLevel={ teamBPlayers[1].skillLevel }
-            />
-        </div>
-        <div
-            style={{
-                position: "absolute",
-                top: "66%",
-                right: "32%"
-            }}
-        >
-            <PlayerIcon
-                playerName={ teamBPlayers[2].name }
-                teamColor={ teamBColor }
-                teamKit={ teamBKit }
-                playerSkillLevel={ teamBPlayers[2].skillLevel }
-            />
-        </div>
-
-        { teamBPlayers.length >= 4 ?
-            <div
-                style={{
-                    position: "absolute",
-                    top: "41%",
-                    right: "32%"
-                }}
-            >
+        { teamBPlayers.map((player, index) => (
+            <div key={ index } className={ "player-b-" + (index + 1) }>
                 <PlayerIcon
-                    playerName={ teamBPlayers[3].name }
+                    playerName={ player.name }
                     teamColor={ teamBColor }
                     teamKit={ teamBKit }
-                    playerSkillLevel={ teamBPlayers[3].skillLevel }
+                    playerSkillLevel={ player.skillLevel }
                 />
             </div>
-        : null }
-
-        { teamBPlayers.length >= 5 ?
-            <div
-                style={{
-                    position: "absolute",
-                    top: "30%",
-                    right: "40%"
-                }}
-            >
-                <PlayerIcon
-                    playerName={ teamBPlayers[4].name }
-                    teamColor={ teamBColor }
-                    teamKit={ teamBKit }
-                    playerSkillLevel={ teamBPlayers[4].skillLevel }
-                />
-            </div>
-        : null }
-
-        { teamBPlayers.length === 6 ?
-            <div
-                style={{
-                    position: "absolute",
-                    top: "54%",
-                    right: "40%"
-                }}
-            >
-                <PlayerIcon
-                    playerName={ teamBPlayers[5].name }
-                    teamColor={ teamBColor }
-                    teamKit={ teamBKit }
-                    playerSkillLevel={ teamBPlayers[5].skillLevel }
-                />
-            </div>
-        : null }
+        )) }
     </div>
 );
 
