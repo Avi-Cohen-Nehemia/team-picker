944b2b72b09bb3133122d21c692a7c77c364b8c8
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-27 17:38:09 +0100

    Updates

---
+++ precache-manifest.a9f8e5b7a51ecd5b8fa7d22ab800c413.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c103cbcffc0968cde988c070436580",
+    "url": "/team-picker/index.html"
+  },
+  {
+    "revision": "0cb03c552c9eb62f85b0",
+    "url": "/team-picker/static/css/main.3f1dc374.chunk.css"
+  },
+  {
+    "revision": "c68aaa02589077b7fe23",
+    "url": "/team-picker/static/js/2.cb60c66d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/team-picker/static/js/2.cb60c66d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb03c552c9eb62f85b0",
+    "url": "/team-picker/static/js/main.cffbb5ea.chunk.js"
+  },
+  {
+    "revision": "1fa314696a4d99ecd615",
+    "url": "/team-picker/static/js/runtime-main.9503ab5e.js"
+  },
+  {
+    "revision": "58966b8a30314879cae9cf03a45bcf19",
+    "url": "/team-picker/static/media/ball.58966b8a.png"
+  },
+  {
+    "revision": "86b40b2413555417c857a7fe97671bc1",
+    "url": "/team-picker/static/media/football.86b40b24.jpg"
+  },
+  {
+    "revision": "8c069787fe1d6c1e299e22783108ad99",
+    "url": "/team-picker/static/media/footballPitch.8c069787.jpg"
+  }
+]);
