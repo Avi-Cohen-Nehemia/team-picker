a7ff653e2aa982613479a4995b57c3ad6c488a4a
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-27 09:17:03 +0100

    majority of pitch-page is now styled by an external css file

--- src/components/FootballPitch/FootballPitch.js
+++ src/components/FootballPitch/FootballPitch.js
@@ -39,15 +39,7 @@ const FootballPitch = ({
             <TeamAPositions/>
             <TeamBPositions/>
         </div>
-        <div
-            className="d-flex justify-content-between align-items-center"
-            style={{
-                position: "absolute",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-            }}
-        >      
+        <div className="d-flex justify-content-between align-items-center pitch-container">      
             <Jumbotron className="team-panel-a">
                 <div className="d-flex flex-column align-items-center justify-content-center">
                     <h1 
@@ -111,10 +103,7 @@ const FootballPitch = ({
                 </div>
             </Jumbotron>
         </div>
-        <div
-            className="d-flex justify-content-center"
-            style={{ paddingTop: "90vh" }}
-        >
+        <div className="pitch-buttons">
             <Button
                 onClick={ handleResetScore }
                 variant="warning"
