736ba4a1f77b489861fc4decf66b3fc21a6b2326
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-28 14:21:50 +0100

    added 'remove player' feature

--- src/components/PlayersTable/index.js
+++ src/components/PlayersTable/index.js
@@ -1,10 +1,18 @@
 import { connect } from "react-redux";
 import PlayersTable from "./PlayersTable";
 
+import { removePlayer } from "./../../data/actions/state";
+
 const mapStateToProps = (state) => {
     return {
         playersPool: state.playersPool,
     };
 };
 
-export default connect(mapStateToProps)(PlayersTable);
+const mapDispatchToProps = dispatch => {
+    return {
+        handleRemovePlayer: (index) => dispatch(removePlayer(index)),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PlayersTable);
