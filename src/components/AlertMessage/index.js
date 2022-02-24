4dc7b810962a4ba87ee699c7b75456b0eb45cde9
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2022-02-24 22:21:04 +0000

    refactor CreatePlayers component

--- src/components/AlertMessage/index.js
+++ src/components/AlertMessage/index.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React from 'react';
 import { useSelector } from 'react-redux';
-import Alert from "react-bootstrap/Alert";
+import Alert from 'react-bootstrap/Alert';
 
 // show a message which its content and color depend on a condition
 const AlertMessage = () => {
@@ -8,8 +8,8 @@ const AlertMessage = () => {
   const isValid = playersPool.length > 5 && playersPool.length % 2 === 0;
 
   return (
-    <Alert variant={isValid ? "success" : "danger"}>
-      {isValid ? "You are ready to continue!" : "Player Pool must have a minimum of 6 players and must be an even number"}
+    <Alert variant={isValid ? 'success' : 'danger'}>
+      {isValid ? 'You are ready to continue!' : 'Player Pool must have a minimum of 6 players and must be an even number'}
     </Alert>
   );
 } ;
--- src/components/CreatePlayers/index.js
+++ src/components/CreatePlayers/index.js
@@ -1,40 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AddPlayer from './../AddPlayer';
+import PlayersTable from './../PlayersTable';
+import AlertMessage from './../AlertMessage';
+import Reset from './../Reset';
+import GenerateTeams from './../GenerateTeams';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-// import local components
-import AddPlayer from "./../AddPlayer";
-import PlayersTable from "./../PlayersTable";
-import AlertMessage from "./../AlertMessage";
-import Reset from "./../Reset";
-import GenerateTeams from "./../GenerateTeams";
-
-// import Bootstrap components
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
-// a component to wraps all of the components that make up the "create-players" page
+// a component to wraps all of the components that make up the 'create-players' page
 const CreatePlayers = () => (
-    <>
-        <AddPlayer/>
-        <AlertMessage/>
-        <PlayersTable/>
-        <Container>
-            <Row>
-                <Col className="justify-content-center d-flex">
-                    <Link to="/confirm-teams">
-                        <GenerateTeams
-                            text={ "Generate Teams" }
-                            color={ "success" }
-                        />
-                    </Link>
-                </Col>
-                <Col className="justify-content-center d-flex">
-                    <Reset text={ "Reset" }/>
-                </Col>
-            </Row>
-        </Container>
-    </>
+  <>
+    <AddPlayer/>
+    <AlertMessage/>
+    <PlayersTable/>
+    <Container>
+      <Row>
+        <Col className='justify-content-center d-flex'>
+          <Link to='/confirm-teams'>
+            <GenerateTeams
+              text={'Generate Teams'}
+              color={'success'}
+            />
+          </Link>
+        </Col>
+        <Col className='justify-content-center d-flex'>
+          <Reset text={'Reset'}/>
+        </Col>
+      </Row>
+    </Container>
+  </>
 );
 
 export default CreatePlayers;
