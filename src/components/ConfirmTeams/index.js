9a5ec04af4c829550e8015d60406c704e65eca97
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2022-02-24 22:29:02 +0000

    refactor ConfirmTeams component

--- src/components/ConfirmTeams/index.js
+++ src/components/ConfirmTeams/index.js
@@ -1,13 +1,77 @@
-import { connect } from "react-redux";
-import ConfirmTeams from "./ConfirmTeams";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Reset from '../Reset';
+import GenerateTeams from '../GenerateTeams';
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 
-const mapStateToProps = (state) => {
-    return {
-        teamA: state.teamA.players,
-        teamB: state.teamB.players,
-        teamASkillLevel: state.teamA.totalSkillLevel,
-        teamBSkillLevel: state.teamB.totalSkillLevel,
-    };
+const ConfirmTeams = () => {
+  const { teamA, teamB } = useSelector((state) => state);
+
+  return (
+    <Container>
+      <Row style={{ paddingTop: '15%' }}>
+        <Col>
+          <Card className='text-center'>
+            <Card.Header>Team A</Card.Header>
+            <ListGroup variant='flush'>
+              { teamA.players.map((player, index) => (
+                <ListGroup.Item
+                  key={index}
+                  className='d-flex justify-content-around'
+                >
+                  <Col>{player.name}</Col>
+                  <Col>{player.skillLevel}</Col>
+                </ListGroup.Item>
+              )) }
+              <ListGroup.Item>Total Skill Level: <strong>{teamA.totalSkillLevel}</strong></ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+        <Col>
+            <Card className='text-center'>
+              <Card.Header>Team B</Card.Header>
+              <ListGroup variant='flush'>
+                { teamB.players.map((player, index) => (
+                  <ListGroup.Item
+                    key={index}
+                    className='d-flex justify-content-around'
+                >
+                    <Col>{player.name}</Col>
+                    <Col>{player.skillLevel}</Col>
+                  </ListGroup.Item>
+                )) }
+                <ListGroup.Item>Total Skill Level: <strong>{teamB.totalSkillLevel}</strong></ListGroup.Item>
+              </ListGroup>
+            </Card>
+        </Col>
+    </Row>
+    <Row className='mt-4'>
+        <Col className='justify-content-center d-flex'>
+          <Link to='/customize-teams'>
+            <Button variant='success'>
+              Confirm Teams
+            </Button>
+          </Link>
+        </Col>
+        <Col className='justify-content-center d-flex'>
+          <GenerateTeams
+            text={'Reshuffle Teams'}
+            color={'warning'}
+          />
+        </Col>
+        <Col className='justify-content-center d-flex'>
+          <Link to='/create-players'>
+            <Reset text={'Start Over'}/>
+          </Link>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
-
-export default connect(mapStateToProps)(ConfirmTeams);
+export default ConfirmTeams;
--- src/components/PlayerIcon/index.js
+++ src/components/PlayerIcon/index.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React from 'react';
+import './../../assets/css/kits.css'
+import kit1 from './../../assets/images/kit1.png';
+import kit2 from './../../assets/images/kit2.png';
+import kit3 from './../../assets/images/kit3.png';
 
-//import styling and images
-import "./../../assets/css/kits.css"
-import kit1 from "./../../assets/images/kit1.png";
-import kit2 from "./../../assets/images/kit2.png";
-import kit3 from "./../../assets/images/kit3.png";
+const PlayerIcon = ({ playerName, teamColor, teamKit, playerSkillLevel }) => {
+  const KITS = {
+    1: kit1,
+    2: kit2,
+    3: kit3,
+  };
 
-const PlayerIcon = ({ playerName, teamColor, teamKit, playerSkillLevel }) => (
-    <span className="d-flex justify-content-center align-items-center flex-column">
-        <span>{ playerSkillLevel }</span>
-        <img
-            src={ teamKit === 1 ? kit1 : (teamKit === 2 ? kit2 : kit3) }
-            style={{ backgroundColor: teamColor }}
-            className="kit-logo"
-            alt="player icon"
-        />
-        <p style={{ backgroundColor: "white" }}>{ playerName }</p>
+  return (
+    <span className='d-flex justify-content-center align-items-center flex-column'>
+      <span>{playerSkillLevel}</span>
+      <img
+        src={KITS[teamKit]}
+        style={{ backgroundColor: teamColor }}
+        className='kit-logo'
+        alt='player icon'
+      />
+      <p style={{backgroundColor: 'white'}}>{ playerName }</p>
     </span>
-);
+  );
+};
 
 export default PlayerIcon;
