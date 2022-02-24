d619e9439630767c14826405a886342df81d1f37
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2022-02-24 22:07:43 +0000

    refactor AlertMessage component

--- src/components/AlertMessage/index.js
+++ src/components/AlertMessage/index.js
@@ -1,10 +1,17 @@
-import { connect } from "react-redux";
-import AlertMessage from "./AlertMessage";
+import React, { useEffect } from "react";
+import { useSelector } from 'react-redux';
+import Alert from "react-bootstrap/Alert";
 
-const mapStateToProps = (state) => {
-    return {
-        conditionsNotMet: state.playersPool.length < 6 || state.playersPool.length % 2 !== 0,
-    };
-};
+// show a message which its content and color depend on a condition
+const AlertMessage = () => {
+  const { playersPool } = useSelector((state) => state);
+  const isValid = playersPool.length > 5 && playersPool.length % 2 === 0;
 
-export default connect(mapStateToProps)(AlertMessage);
+  return (
+    <Alert variant={isValid ? "success" : "danger"}>
+      {isValid ? "You are ready to continue!" : "Player Pool must have a minimum of 6 players and must be an even number"}
+    </Alert>
+  );
+} ;
+
+export default AlertMessage;
