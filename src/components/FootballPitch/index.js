6a965f3056068249e7cecfd2c22133bd54be6b7e
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2022-03-27 21:43:41 +0100

    refactor football pitch component

--- src/components/FootballPitch/index.js
+++ src/components/FootballPitch/index.js
@@ -1,29 +1,128 @@
-import { connect } from "react-redux";
-import FootballPitch from "./FootballPitch";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+	incrementTeamAScore,
+	incrementTeamBScore,
+	resetScore
+} from "../../data/actions/state";
+import Reset from './../Reset';
+import TeamPositions from '../TeamPositions';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+import './../../assets/css/pitch-page.css';
+import footballPitch from './../../assets/images/footballPitch.jpg';
+import kit1 from './../../assets/images/kit1.png';
+import kit2 from './../../assets/images/kit2.png';
+import kit3 from './../../assets/images/kit3.png';
 
-import { incrementTeamAScore, incrementTeamBScore, resetScore } from "../../data/actions/state";
+// a component to wraps all of the components that make up the 'football-pitch' page
+const FootballPitch = () => {
+	const {	teamA, teamB } = useSelector((state) => state);
+	const dispatch = useDispatch();
 
-const mapStateToProps = (state) => {
-    return {
-        teamAPlayers: state.teamA.players,
-        teamBPlayers: state.teamB.players,
-        teamAName: state.teamA.name,
-        teamBName: state.teamB.name,
-        teamAKit: state.teamA.kit,
-        teamBKit: state.teamB.kit,
-        teamAColor: state.teamA.color,
-        teamBColor: state.teamB.color,
-        teamAScore: state.teamA.score,
-        teamBScore: state.teamB.score,
-    };
-};
+	const handleTeamAScore = () => {
+		dispatch(incrementTeamAScore());
+	};
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        handleTeamAScore: () => dispatch(incrementTeamAScore()),
-        handleTeamBScore: () => dispatch(incrementTeamBScore()),
-        handleResetScore: () => dispatch(resetScore())
-    }
+	const handleTeamBScore = () => {
+		dispatch(incrementTeamBScore());
+	};
+
+	const handleResetScore = () => {
+		dispatch(resetScore());
+	};
+
+	return (
+		<>
+			<div
+				style={{
+					position: 'absolute',
+					top: '50%',
+					left: '50%',
+					transform: 'translate(-50%, -50%)',
+					zIndex: '2',
+					height: '60%',
+					width: '50%'
+				}}
+			>
+			<TeamPositions
+				teamPlayers={teamA.players}
+				teamColor={teamA.color}
+				teamKit={teamA.kit}
+				team={'a'}
+			/>
+			<TeamPositions
+				teamPlayers={teamB.players}
+				teamColor={teamB.color}
+				teamKit={teamB.kit}
+				team={'b'}
+			/>
+		</div>
+		<div className='pitch-container'>      
+			<Jumbotron className='team-panel-a'>
+				<div className='d-flex flex-column align-items-center justify-content-center'>
+					<h1 
+						className='px-3 mb-4 text-center'
+						style={{backgroundColor: teamA.color}}
+					>
+						{teamA.name}
+					</h1>
+					<img
+						src={teamA.kit === 1 ? kit1 : (teamA.kit === 2 ? kit2 : kit3) }
+						alt='team kit'
+						className='team-kit'
+						style={{backgroundColor: teamA.color}}
+					/>
+					<h1 className='mb-3 text-white'>{ teamA.score }</h1>
+					<Button
+						onClick={ handleTeamAScore }
+						className='px-5'
+						variant='success'
+					>
+						+
+					</Button>
+				</div>
+			</Jumbotron>
+			<img src={footballPitch} alt='football pitch' />
+			<Jumbotron className='team-panel-b'>
+				<div className='d-flex flex-column align-items-center justify-content-center'>
+					<h1 
+						className='px-3 mb-4 text-center'
+						style={{backgroundColor: teamB.color}}
+					>
+						{teamB.name}
+					</h1>
+					<img
+						src={ teamB.kit === 1 ? kit1 : (teamB.kit === 2 ? kit2 : kit3) }
+						alt='team kit'
+						className='team-kit'
+						style={{backgroundColor: teamB.color}}
+					/>
+					<h1 className='mb-3 text-white'>
+						{ teamB.score }
+					</h1>
+					<Button
+						onClick={ handleTeamBScore }
+						className='px-5'
+						variant='success'
+					>
+						+
+					</Button>
+				</div>
+			</Jumbotron>
+			</div>
+			<div className='pitch-buttons'>
+				<Button
+					onClick={ handleResetScore }
+					variant='warning'
+					className='mr-3'
+				>
+					Reset Score
+				</Button>
+				<Reset className='ml-3'>Start Over</Reset>
+			</div>
+		</>
+	);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(FootballPitch);
+export default FootballPitch;
