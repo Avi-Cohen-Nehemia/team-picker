60eaa67e72416b09952a0b6ccad2c9d52049aa23
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2022-03-28 20:37:47 +0100

    fix indetentaion in reducer file

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -3,183 +3,182 @@ import initialState from "./initialState";
 
 // adding the new player to the playersPool
 const addPlayer = (state, action) => {
-    if (state.playersPool.length < 12) {
-        return {
-            ...state,
-            playersPool: [...state.playersPool, action.player]
-        }
-    } else {
-        return {
-            ...state,
-            playersPool: [...state.playersPool],
-        }
-    }
+	if (state.playersPool.length < 12) {
+		return {
+			...state,
+			playersPool: [...state.playersPool, action.player]
+		}
+	} else {
+		return {
+			...state,
+			playersPool: [...state.playersPool],
+		}
+	}
 };
 
 // remove a specific player
 const removePlayer = (state, { index }) => {
-    return {
-        ...state,
-        playersPool: state.playersPool.filter((_, i) => i !== index)
-    }
+	return {
+		...state,
+		playersPool: state.playersPool.filter((_, i) => i !== index)
+	}
 };
 
 // shuffle the players pool
 const shufflePlayers = (state) => {
-    return {
-        ...state,
-        playersPool: [...state.playersPool.sort(() => Math.random() - 0.5)]
-    }
+	return {
+		...state,
+		playersPool: [...state.playersPool.sort(() => Math.random() - 0.5)]
+	}
 }
-
 
 // generate the teams and store them in state
 const generateTeams = (state) => {
 
-    let firstHalf = state.playersPool.filter((_, index) => index < state.playersPool.length / 2);
-    let secondHalf = state.playersPool.filter((_, index) => index >= state.playersPool.length / 2);
+	let firstHalf = state.playersPool.filter((_, index) => index < state.playersPool.length / 2);
+	let secondHalf = state.playersPool.filter((_, index) => index >= state.playersPool.length / 2);
 
-    return {
-        ...state,
-        teamA: {
-            ...state.teamA,
-            players: [...firstHalf],
-        },
-        teamB: {
-            ...state.teamB,
-            players: [...secondHalf],
-        }
-    }
+	return {
+		...state,
+		teamA: {
+			...state.teamA,
+			players: [...firstHalf],
+		},
+		teamB: {
+			...state.teamB,
+			players: [...secondHalf],
+		}
+	}
 }
 
 // calculate the teams total skill level
 const totalSkillLevel = (state) => {
     
-    let teamASkill = state.teamA.players.reduce((acc, player) => {
-        return acc + player.skillLevel.length
-    }, 0)
+	let teamASkill = state.teamA.players.reduce((acc, player) => {
+		return acc + player.skillLevel.length
+	}, 0)
 
-    let teamBSkill = state.teamB.players.reduce((acc, player) => {
-        return acc + player.skillLevel.length
-    }, 0)
+	let teamBSkill = state.teamB.players.reduce((acc, player) => {
+		return acc + player.skillLevel.length
+	}, 0)
 
-    return {
-        ...state,
-        teamA: {
-            ...state.teamA,
-            totalSkillLevel: teamASkill,
-        },
-        teamB: {
-            ...state.teamB,
-            totalSkillLevel: teamBSkill,
-        }
-    }
+	return {
+		...state,
+		teamA: {
+			...state.teamA,
+			totalSkillLevel: teamASkill,
+		},
+		teamB: {
+			...state.teamB,
+			totalSkillLevel: teamBSkill,
+		}
+	}
 }
 
 const selectTeamAKit = (state, action) => {
-    return {
-        ...state,
-        teamA: {
-            ...state.teamA,
-            kit: action.teamA.kit
-        },
-    }
+	return {
+		...state,
+		teamA: {
+			...state.teamA,
+			kit: action.teamA.kit
+		},
+	}
 }
 
 const selectTeamBKit = (state, action) => {
-    return {
-        ...state,
-        teamB: {
-            ...state.teamB,
-            kit: action.teamB.kit
-        },
-    }
+	return {
+		...state,
+		teamB: {
+			...state.teamB,
+			kit: action.teamB.kit
+		},
+	}
 }
 
 const selectTeamAColor = (state, action) => {
-    return {
-        ...state,
-        teamA: {
-            ...state.teamA,
-            color: action.teamA.color
-        },
-    }
+	return {
+		...state,
+		teamA: {
+			...state.teamA,
+			color: action.teamA.color
+		},
+	}
 }
 
 const selectTeamBColor = (state, action) => {
-    return {
-        ...state,
-        teamB: {
-            ...state.teamB,
-            color: action.teamB.color
-        },
-    }
+	return {
+		...state,
+		teamB: {
+			...state.teamB,
+			color: action.teamB.color
+		},
+	}
 }
 
 const setTeamsNames = (state, action) => {
-    return {
-        ...state,
-        teamA: {
-            ...state.teamA,
-            name: action.teamA.name
-        },
-        teamB: {
-            ...state.teamB,
-            name: action.teamB.name
-        },
-    }
+	return {
+		...state,
+		teamA: {
+			...state.teamA,
+			name: action.teamA.name
+		},
+		teamB: {
+			...state.teamB,
+			name: action.teamB.name
+		},
+	}
 }
 
 const incrementTeamAScore = (state) => {
-    return {
-    ...state,
-        teamA: {
-            ...state.teamA,
-            score: +state.teamA.score + 1
-        },
-    }
+	return {
+		...state,
+		teamA: {
+				...state.teamA,
+				score: +state.teamA.score + 1
+		},
+	}
 }
 
 const incrementTeamBScore = (state) => {
-    return {
-    ...state,
-        teamB: {
-            ...state.teamB,
-            score: +state.teamB.score + 1
-        },
-    }
+	return {
+		...state,
+		teamB: {
+			...state.teamB,
+			score: +state.teamB.score + 1
+		},
+	}
 }
 
 const resetScore = (state) => {
-    return {
-    ...state,
-        teamA: {
-            ...state.teamA,
-            score: 0
-        },
-        teamB: {
-            ...state.teamB,
-            score: 0
-        },
-    }
+	return {
+		...state,
+		teamA: {
+			...state.teamA,
+			score: 0
+		},
+		teamB: {
+			...state.teamB,
+			score: 0
+		},
+	}
 }
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case "ADD_PLAYER": return addPlayer(state, action);
-        case "REMOVE_PLAYER": return removePlayer(state, action);
-        case "GENERATE_TEAMS": return totalSkillLevel(generateTeams(shufflePlayers(state)));
-        case "SELECT_TEAM_A_KIT": return selectTeamAKit(state, action);
-        case "SELECT_TEAM_B_KIT": return selectTeamBKit(state, action);
-        case "SELECT_TEAM_A_COLOR": return selectTeamAColor(state, action);
-        case "SELECT_TEAM_B_COLOR": return selectTeamBColor(state, action);
-        case "SET_TEAMS_NAMES": return setTeamsNames(state, action);
-        case "INCREMENT_TEAM_A_SCORE": return incrementTeamAScore(state);
-        case "INCREMENT_TEAM_B_SCORE": return incrementTeamBScore(state);
-        case "RESET_SCORE": return resetScore(state);
-        case "RESET_GAME": return initialState;
-        default: return state;
-    }
+	switch (action.type) {
+		case "ADD_PLAYER": return addPlayer(state, action);
+		case "REMOVE_PLAYER": return removePlayer(state, action);
+		case "GENERATE_TEAMS": return totalSkillLevel(generateTeams(shufflePlayers(state)));
+		case "SELECT_TEAM_A_KIT": return selectTeamAKit(state, action);
+		case "SELECT_TEAM_B_KIT": return selectTeamBKit(state, action);
+		case "SELECT_TEAM_A_COLOR": return selectTeamAColor(state, action);
+		case "SELECT_TEAM_B_COLOR": return selectTeamBColor(state, action);
+		case "SET_TEAMS_NAMES": return setTeamsNames(state, action);
+		case "INCREMENT_TEAM_A_SCORE": return incrementTeamAScore(state);
+		case "INCREMENT_TEAM_B_SCORE": return incrementTeamBScore(state);
+		case "RESET_SCORE": return resetScore(state);
+		case "RESET_GAME": return initialState;
+		default: return state;
+	}
 };
 
 export default reducer;
